bdb14a342ace7bb0fbdc5b62981864ae147a5e2e
rlin2k1 <rlin2k1@gmail.com>
2020-05-21 01:25:30 -0700

    Adds proxy configuration to express server.

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -5,5 +5,104 @@ import { Injectable } from '@angular/core';
 })
 export class BlogService {
 
+  private blogUrl = 'api';  // URL to web api
+
+  draft: Post // Saving a past post
+
   constructor() { }
+
+  jsonToPostArray(json) {
+    let result: Post[] = [];
+
+    for(var i in json)
+      result.push(new Post(i));
+
+    return result;
+  }
+
+  fetchPosts(username: string): Promise<Post[]> {
+    const url = `${this.blogUrl}/${username}`;
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      let res = this.jsonToPostArray(json);
+      return res;
+    })
+    .catch();
+  }
+
+  getPost(username: string, postid: number): Promise<Post> {
+    const url = `${this.blogUrl}/${username}/${postid}`;
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      let res = this.jsonToPostArray(json);
+      return res;
+    })
+    .catch();
+  }
+
+  newPost(username: string, post: Post): Promise<void> {
+    // Need to calculate updated postid using post.postid
+    const url = `${this.blogUrl}/${username}/${post.postid}`;
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      let res = this.jsonToPostArray(json);
+      return res;
+    })
+    .catch();
+  }
+
+  updatePost(username: string, post: Post): Promise<void> {
+    const url = `${this.blogUrl}/${username}/${post.postid}`;
+
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      let res = this.jsonToPostArray(json);
+      return res;
+    })
+    .catch();
+  }
+
+  deletePost(username: string, postid: number): Promise<void> {
+    const url = `${this.blogUrl}/${username}/${postid}`;
+    
+    fetch(url)
+    .then((res) => res.json())
+    .then((json) => {
+      let res = this.jsonToPostArray(json);
+      return res;
+    })
+    .catch();
+  }
+
+  setCurrentDraft(post: Post): void {
+    this.draft = post;
+    return;
+  }
+
+  getCurrentDraft(): Post {
+    return this.draft;
+  }
+}
+
+export class Post {
+  postid: number;
+  created: Date;
+  modified: Date;
+  title: string;
+  body: string;
+
+  constructor(jsonObject) {
+    this.postid = jsonObject.postid;
+    this.created = jsonObject.created;
+    this.modified = jsonObject.modified;
+    this.title = jsonObject.title;
+    this.body = jsonObject.body;
+  }
 }
