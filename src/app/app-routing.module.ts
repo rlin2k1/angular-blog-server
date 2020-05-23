22a669338901b2797f2e5511cb93700c3be3889e
rlin2k1 <rlin2k1@gmail.com>
2020-05-23 00:42:08 -0700

    Adds router outlet to app root.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';\
+import { EditComponent } from './edit/edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'edit/:id', component: EditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash: true})],
